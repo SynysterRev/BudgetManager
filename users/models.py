5dff2646cdc72e89d2f5a843a5fd578798928330
Michael <michael.arn@live.fr>
2025-05-16 17:11:16 +0200

    create project

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    """
+    Custom user model that extends the AbstractUser model.
+    """
+
+    def __str__(self):
+        return self.username
