2fc615db95063df2312abf7200a200efaeaa1bd0
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-06-04 16:10:32 +0200

    fix build files and json package to build output css when deploying

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+npm install
+
+# Générer le CSS Tailwind
+npm run build-css
+
 python3 -m pip install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
